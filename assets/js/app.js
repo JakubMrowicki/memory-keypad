901fbfc37180d1239658a3e3610a77b3571e132c
JakubMrowicki <jakubmrowicki96@gmail.com>
2021-04-13 20:28:02 +0000

    add javascript layout and variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,67 @@
-let memoryKeypad = {
-    computedPattern: [],
+let game = {
 
-    getComputedPattern(length) {
-        let request = new Request('https://www.random.org/integers/?num=' + length + '&min=1&max=9&col=1&base=10&format=plain&rnd=new');
-        fetch(request)
-            .then(function (response) {
-                return response.text().then(function (text) {
-                    let numberArray = Array.from(String(text), Number);
-                    let filtered = numberArray.filter(function (el) { //Credit: Christian C. Salvadó https://stackoverflow.com/a/281335
-                        return el > 0;
-                    });
-                    memoryKeypad.computedPattern = filtered;
-                })
-            });
+    var: {
+        pattern: [],
+        input: [],
+        difficulty: 2,
+        position: 1,
+        live: false,
+        keypause: true,
     },
+
+    ui: {
+        keypad: $('.keypad')[0],
+        keys: $('.keypad')[0].children,
+        button: $('.btn')[0],
+        difficulty: $('#difficultySelect')[0],
+        score: $('#score'),
+        highscore: $('#highscore'),
+    },
+
+    anim: {
+        light() { //Animation When Key Pressed
+
+        },
+
+        win() { //Animation When Game Won
+
+        },
+
+        loss() { //Animation When Game Lost
+
+        },
+
+        pattern() { //Animation For Playing Pattern
+
+        }
+    },
+
+    task: {
+        start(difficulty) { //Start the game using difficulty
+
+        },
+
+        play() { //Runs on key click, sends clicks for validation
+
+        },
+
+        validate() { //Check player input
+
+        },
+
+        pattern(length) { //Get Random Number Of Length Of length from random.org api
+            let request = new Request('https://www.random.org/integers/?num=' + length + '&min=1&max=9&col=1&base=10&format=plain&rnd=new');
+            fetch(request)
+                .then(function (response) {
+                    return response.text().then(function (text) {
+                        let array = Array.from(String(text), Number);
+                        array = array.filter(function (el) { //Credit: Christian C. Salvadó https://stackoverflow.com/a/281335
+                            return el > 0;
+                        });
+                        game.var.pattern = array;
+                    });
+                });
+        }
+    },
+
 };
